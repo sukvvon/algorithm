707c3bb5d9ebe1e6b6dfe0ddebbfc41abc74326f
sukvvon <sukvvon@gmail.com>
2022-09-21 19:55:05 +0900

    feat: add Programmers Lv.1 문자열 다루기 기본

---
+++ src/Programmers/Lv.1/문자열_다루기_기본/solution.js
@@ -0,0 +1,39 @@
+/*
+문자열 다루기 기본
+
+문제 설명
+문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+제한 사항
+s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
+
+입출력 예
+s	return
+"a234"	false
+"1234"	true
+문제가 잘 안풀린다면😢
+힌트가 필요한가요? [코딩테스트 연습 힌트 모음집]으로 오세요! → 클릭
+
+공지 - 2022년 7월 22일 테스트케이스가 추가되었습니다.
+공지 - 2022년 7월 22일 제한 사항이 추가되었습니다.
+*/
+
+function solution(s) {
+  let answer = true;
+
+  if (s.length === 4 || s.length === 6) {
+    const inputs = s.split("");
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].charCodeAt() >= 65) {
+        answer = false;
+        break;
+      }
+    }
+  } else {
+    answer = false;
+  }
+
+  return answer;
+}
