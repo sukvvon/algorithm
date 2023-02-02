c0f35347ecdcf3d47c16702ffa97159023f4e63d
sukvvon <sukvvon@gmail.com>
2023-02-02 22:46:43 +0900

    refactor: clean up solution in Programmers Lv.1 나머지가 1이 되는 수 찾기

--- src/Programmers/Lv.1/나머지가_1이_되는_수_찾/solution.js
+++ src/Programmers/Lv.1/나머지가_1이_되는_수_찾/solution.js
@@ -23,14 +23,9 @@ n	result
 */
 
 function solution(n) {
-  let answer = 0;
-
   for (let i = 1; i < n; i++) {
     if (n % i === 1) {
-      answer = i;
-      break;
+      return i;
     }
   }
-
-  return answer;
 }
