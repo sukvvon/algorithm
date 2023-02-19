379a1760e6900f34af0ddb8ff742587df61e4c5c
sukvvon <sukvvon@gmail.com>
2023-02-19 13:46:09 +0900

    feat: add solution in Programmers Lv.2 스킬트리

---
+++ src/Programmers/Lv.2/스킬트리/solution.js
@@ -0,0 +1,91 @@
+/*
+스킬트리
+
+문제 설명
+선행 스킬이란 어떤 스킬을 배우기 전에 먼저 배워야 하는 스킬을 뜻합니다.
+
+예를 들어 선행 스킬 순서가 스파크 → 라이트닝 볼트 → 썬더일때, 썬더를 배우려면 먼저 라이트닝 볼트를 배워야 하고, 라이트닝 볼트를 배우려면 먼저 스파크를 배워야 합니다.
+
+위 순서에 없는 다른 스킬(힐링 등)은 순서에 상관없이 배울 수 있습니다. 따라서 스파크 → 힐링 → 라이트닝 볼트 → 썬더와 같은 스킬트리는 가능하지만, 썬더 → 스파크나 라이트닝 볼트 → 스파크 → 힐링 → 썬더와 같은 스킬트리는 불가능합니다.
+
+선행 스킬 순서 skill과 유저들이 만든 스킬트리1를 담은 배열 skill_trees가 매개변수로 주어질 때, 가능한 스킬트리 개수를 return 하는 solution 함수를 작성해주세요.
+
+제한 조건
+스킬은 알파벳 대문자로 표기하며, 모든 문자열은 알파벳 대문자로만 이루어져 있습니다.
+스킬 순서와 스킬트리는 문자열로 표기합니다.
+예를 들어, C → B → D 라면 "CBD"로 표기합니다
+선행 스킬 순서 skill의 길이는 1 이상 26 이하이며, 스킬은 중복해 주어지지 않습니다.
+skill_trees는 길이 1 이상 20 이하인 배열입니다.
+skill_trees의 원소는 스킬을 나타내는 문자열입니다.
+skill_trees의 원소는 길이가 2 이상 26 이하인 문자열이며, 스킬이 중복해 주어지지 않습니다.
+
+입출력 예
+skill	skill_trees	return
+"CBD"	["BACDE", "CBADF", "AECB", "BDA"]	2
+
+입출력 예 설명
+"BACDE": B 스킬을 배우기 전에 C 스킬을 먼저 배워야 합니다. 불가능한 스킬트립니다.
+"CBADF": 가능한 스킬트리입니다.
+"AECB": 가능한 스킬트리입니다.
+"BDA": B 스킬을 배우기 전에 C 스킬을 먼저 배워야 합니다. 불가능한 스킬트리입니다.
+스킬 트리: 유저가 스킬을 배울 순서 ↩
+*/
+
+// 1
+function solution(skill, skill_trees) {
+  let answer = 0;
+
+  function checked(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== i) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  for (const tree of skill_trees) {
+    const check = [...tree]
+      .filter((val) => skill.includes(val))
+      .map((val) => skill.indexOf(val));
+
+    if (checked(check)) {
+      answer++;
+    }
+  }
+
+  return answer;
+}
+
+// 2
+function solution(skill, skill_trees) {
+  function isValid(tree) {
+    const skills = [...skill];
+
+    for (let i = 0; i < tree.length; i++) {
+      if (!skill.includes(tree[i])) {
+        continue;
+      }
+
+      if (skills.shift() === tree[i]) {
+        continue;
+      }
+
+      return false;
+    }
+
+    return true;
+  }
+
+  return skill_trees.filter(isValid).length;
+}
+
+// 3
+function solution(skill, skill_trees) {
+  const re = new RegExp(`[^${skill}]`, "g");
+
+  return skill_trees
+    .map((value) => value.replace(re, ""))
+    .filter((value) => skill.indexOf(value) === 0 || value === "").length;
+}
