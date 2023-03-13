a231c9e6cbe420205d4d00e86794d866af253452
sukvvon <sukvvon@gmail.com>
2023-03-13 23:06:31 +0900

    feat: add solution in Baekjoon bronze 피보나치의 수 5

---
+++ src/Baekjoon/bronze/피보나치의_수_5/n10870.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const input = +fs.readFileSync(
+  process.platform === "linux" ? "/dev/stdin" : "../../../index.txt"
+);
+
+function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(input));
