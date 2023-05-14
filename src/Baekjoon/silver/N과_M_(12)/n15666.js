63177c3743d0a585996608776dd345a38284a3c5
sukvvon <sukvvon@gmail.com>
2023-05-14 14:04:43 +0900

    feat: add solution in Baekjoon silver N과 M (12)

---
+++ src/Baekjoon/silver/N과_M_(12)/n15666.js
@@ -0,0 +1,98 @@
+/*
+N과 M (12)
+
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+2 초	512 MB	13831	11008	9514	80.764%
+
+문제
+N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+N개의 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+고른 수열은 비내림차순이어야 한다.
+길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+
+입력
+첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+예제 입력 1 
+3 1
+4 4 2
+
+예제 출력 1 
+2
+4
+
+예제 입력 2 
+4 2
+9 7 9 1
+
+예제 출력 2 
+1 1
+1 7
+1 9
+7 7
+7 9
+9 9
+
+예제 입력 3 
+4 4
+1 1 2 2
+
+예제 출력 3 
+1 1 1 1
+1 1 1 2
+1 1 2 2
+1 2 2 2
+2 2 2 2
+
+출처
+문제를 만든 사람: baekjoon
+데이터를 추가한 사람: chogahui05, thak00
+
+알고리즘 분류
+백트래킹
+*/
+
+const fs = require("fs");
+
+const [[n, m], arr] = fs
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : "../../../index.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map(Number));
+
+function solution(m, arr) {
+  const answer = [];
+  const sortedArr = [...new Set(arr)].sort((a, b) => a - b);
+
+  function dfs(cnt, curArr, leftArr) {
+    if (cnt === m) {
+      answer.push(curArr.join(" "));
+    } else {
+      leftArr.forEach((v) => {
+        dfs(
+          cnt + 1,
+          [...curArr, v],
+          leftArr.filter((lv) => lv >= v)
+        );
+      });
+    }
+  }
+
+  dfs(0, [], sortedArr);
+
+  return answer.join("\n");
+}
+
+console.log(solution(m, arr));
